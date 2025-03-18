409a80ff8289d860ce2cdb8c624ebac6d83960ae
divya patel <divya.patel@moschip.com>
2025-03-18 23:17:12 +0530

    Added some required changes to setup alembic for keeping history.

---
+++ Backend/src/config/config_db.py
@@ -0,0 +1,17 @@
+import os
+from os import getenv
+
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+load_dotenv()
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+
+    model_config = SettingsConfigDict(
+        env_file="../../.env",
+        extra="ignore"
+    )
+
+Config = Settings()
