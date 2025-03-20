1d043e6033fb9d766d19ff77be05da7b6b83ed29
Divya Patel <pateldivya1804@gmail.com>
2025-03-20 21:53:16 +0530

    Completed a task of creating a user table inside the postgresql using alembic.

---
+++ Backend/src/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from src.db.main import init_db
+
+@asynccontextmanager
+async def life_span(app:FastAPI):
+    print("Server is started...")
+    await init_db()
+    yield
+    print("Server is stopped...")
+
+version = "v1"
+
+app = FastAPI(
+    title="Canteen Management System",
+    description="A large scale project to phase and solve all the new deficulties.",
+    version=version,
+    lifespan=life_span
+)
+# app.include_router(book_router, prefix=f"/api/{version}/books")
+
+# Command to run the Application...
+# ------------------------------------
+# fastapi dev src/main.py --port 5005
